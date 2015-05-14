b772e8617f03079af9b915225168d9cde063854b
Volker Braun <vbraun.name@gmail.com>
2015-05-14 11:14:00 +0200

    do not use global fabric env

--- git_trac/releasemgr/google_compute_engine.py
+++ git_trac/releasemgr/google_compute_engine.py
@@ -5,20 +5,19 @@ import os
 
 try:
     import fabric
+    import fabric.tasks
     from fabric.api import env, run, sudo, put, settings, cd
     from fabric.contrib.files import exists
 except ImportError:
     # Fabric shoud be py3-compatible any time now, but not yet
-    # Evil hack to make importable in py3 tests
-    class AttrDict(dict):
-        def __init__(self, *args, **kwargs):
-            super(AttrDict, self).__init__(*args, **kwargs)
-            self.__dict__ = self
-    env = AttrDict()
+    pass
 
-env.use_ssh_config = True
-env.user = 'sagemath'
-env.hosts = ['google_compute_engine']
+
+env_gce = dict(
+    use_ssh_config=True,
+    user='sagemath',
+    host_string='google_compute_engine',
+)
 
 
 def package_name(url_or_path):
@@ -30,22 +29,23 @@ def upload_tarball(url_or_path):
     """
     Add tarball to http://sagemath.org/packages/upstream
     """
-    package = package_name(url_or_path)
-    destination = os.path.join('/home/sagemath/files/spkg/upstream', package)
-    if os.path.exists(url_or_path):        # is local file
-        put(url_or_path, os.path.join(destination, os.path.basename(url_or_path)))
-    else:                                  # should be a url
-        with cd(destination):
-            run('wget --no-directories -p -N ' + url_or_path)
-    run('/home/sagemath/publish-files.sh')
+    with settings(**env_gce):
+        package = package_name(url_or_path)
+        destination = os.path.join('/home/sagemath/files/spkg/upstream', package)
+        if os.path.exists(url_or_path):        # is local file
+            put(url_or_path, os.path.join(destination, os.path.basename(url_or_path)))
+        else:                                  # should be a url
+            with cd(destination):
+                run('wget --no-directories -p -N ' + url_or_path)
+        run('/home/sagemath/publish-files.sh')
 
 
-    
 def upload_dist_tarball(tarball):
     """
     Add tarball to http://sagemath.org/packages/upstream
     """
-    basename = os.path.basename(tarball)
-    put(tarball, os.path.join('/home/sagemath/files/devel', basename))
-    run('/home/sagemath/publish-files.sh')
+    with settings(**env_gce):
+        basename = os.path.basename(tarball)
+        put(tarball, os.path.join('/home/sagemath/files/devel', basename))
+        run('/home/sagemath/publish-files.sh')
 
--- git_trac/releasemgr/sagepad_org.py
+++ git_trac/releasemgr/sagepad_org.py
@@ -9,21 +9,19 @@ try:
     from fabric.contrib.files import exists
 except ImportError:
     # Fabric shoud be py3-compatible any time now, but not yet
-    # Evil hack to make importable in py3 tests
-    class AttrDict(dict):
-        def __init__(self, *args, **kwargs):
-            super(AttrDict, self).__init__(*args, **kwargs)
-            self.__dict__ = self
-    env = AttrDict()
+    pass
 
-env.use_ssh_config = True
-env.user = 'files'
-env.hosts = ['sagepad_org']
-env.host_string = 'sagepad_org'
+
+env_sagepad = dict(
+    use_ssh_config=True,
+    user='files',
+    host_string='sagepad_org'
+)
 
 
 def rsync_upstream_packages():
     """
     pull upstream packages via rsync
     """
-    run('rsync --archive --recursive rsync.sagemath.org::spkgs/upstream /home/files-pub')
+    with settings(**env_sagepad):
+        run('rsync --archive --recursive rsync.sagemath.org::spkgs/upstream /home/files-pub')
